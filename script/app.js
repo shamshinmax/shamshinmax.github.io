7a473fdceb3493f5d2e6589a6306dbf2f991a7cd
shamshinmax <94185311+shamshinmax@users.noreply.github.com>
2023-04-13 23:27:30 +0300

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,14 +1,9 @@
-let tg = window.Telegram.WebApp;
 
-tg.expand();
-tg.sendData("sendTestMessage");
-tg.MainButton.textColor = "#FFFFFF";
-tg.MainButton.color = "#FF00FF";
 
 let btn = document.getElementById("btn");
 
 btn.addEventListener("click", function(){
 
-    tg.MainButton.show();
+    console.log("ok)")
     
 });
