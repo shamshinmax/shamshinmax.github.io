d6e4916a6de6195dd586d970b738ef325fff4d9d
shamshinmax <94185311+shamshinmax@users.noreply.github.com>
2023-04-14 00:11:58 +0300

    Update app.js

--- script/app.js
+++ script/app.js
@@ -5,12 +5,9 @@ const times = document.getElementById("startTime");
 
 times.addEventListener("input", () => {
     time = times.value;
-    console.log(time)
 }, false);
 
 btn.addEventListener("click", function(){
     var place = document.getElementById("selection").value;
-    console.log(time+" "+place);
-    tg.answerWebAppQuery("123")
     tg.sendData(time+" "+place);
 });
