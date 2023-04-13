535b764f481e173338bb0a2135c35cf72ff3f1a7
shamshinmax <94185311+shamshinmax@users.noreply.github.com>
2023-04-13 23:01:32 +0300

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,5 +1,14 @@
 let tg = window.Telegram.WebApp;
+
 tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#FF00FF";
+
+let btn = document.getElementById("btn");
+
 btn.addEventListener("click", function(){
-    tg.sendData("123");
+    tg.MainButton.setText("Сообщение отправлено!");
+    tg.MainButton.show();
+    tg.sendData("sendTestMessage");
 });
