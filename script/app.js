69530936ab15e0799cae649bad57ac8ba6067d73
shamshinmax <94185311+shamshinmax@users.noreply.github.com>
2023-04-13 23:24:17 +0300

    Update app.js

--- script/app.js
+++ script/app.js
@@ -8,7 +8,7 @@ tg.MainButton.color = "#FF00FF";
 let btn = document.getElementById("btn");
 
 btn.addEventListener("click", function(){
-    tg.MainButton.setText("Сообщение отправлено!");
+
     tg.MainButton.show();
     
 });
