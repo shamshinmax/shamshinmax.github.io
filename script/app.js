aeb929e0e84e731454729e837ccaadc8fe0d5922
shamshinmax <94185311+shamshinmax@users.noreply.github.com>
2023-04-14 22:08:01 +0300

    Update app.js

--- script/app.js
+++ script/app.js
@@ -3,7 +3,16 @@ tg.setBackgroundColor("#ffffff")
 var time = 0;
 const date = new Date();
 const timeControl = document.querySelector('input[type="time"]');
-timeControl.value = date.getHours() + ":" + date.getMinutes();;
+let hours = date.getHours();
+let minutes = date.getMinutes();;
+if (date.getMinutes() < 10){
+    minutes = "0" + date.getMinutes();
+}
+if (date.getHours() < 10){
+   hours = "0" + date.getHours();
+}
+console.log()
+timeControl.value = hours + ":" + minutes;
 let btn = document.getElementById("btn");
 const times = document.getElementById("startTime");
 times.addEventListener("input", () => {
