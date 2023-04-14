05c2f749342bb6c54e2e0babc52a9158abd558f6
shamshinmax <94185311+shamshinmax@users.noreply.github.com>
2023-04-14 18:26:00 +0300

    Update app.js

--- script/app.js
+++ script/app.js
@@ -5,7 +5,11 @@ const times = document.getElementById("startTime");
 times.addEventListener("input", () => {
     time = times.value;
 }, false);
-
+if (tg.colorScheme = "dark") {
+    tg.setBackgroundColor("#001a26")
+} else if {
+    tg.setBackgroundColor("#FFFFFF")
+}
 btn.addEventListener("click", function(){
     var place = document.getElementById("selection").value;
     tg.sendData(time+" "+place+" "+tg.colorScheme);
