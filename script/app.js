2417d36dece0a229f6a54c1dca62e534d7e4d893
shamshinmax <94185311+shamshinmax@users.noreply.github.com>
2023-04-14 20:53:28 +0300

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,19 +1,12 @@
 let tg = window.Telegram.WebApp;
-const {
-  bg_color,
-  text_color,
-  hint_color,
-  button_color,
-  button_text_color,
-  secondary_bg_color,
-} = Telegram.WebApp.themeParams
+
 var time = 0;
 let btn = document.getElementById("btn");
 const times = document.getElementById("startTime");
 times.addEventListener("input", () => {
     time = times.value;
 }, false);
-Telegram.WebApp.setBackgroundColor('bg_color');
+
 
 btn.addEventListener("click", function(){
     var place = document.getElementById("selection").value;
