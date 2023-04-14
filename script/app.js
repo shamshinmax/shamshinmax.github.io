4f8bdecc504f129bb3fdeeb7efd14098d5c96e61
shamshinmax <94185311+shamshinmax@users.noreply.github.com>
2023-04-14 13:51:11 +0300

    Update app.js

--- script/app.js
+++ script/app.js
@@ -2,12 +2,11 @@ let tg = window.Telegram.WebApp;
 var time = 0;
 let btn = document.getElementById("btn");
 const times = document.getElementById("startTime");
-console.log(tg.secondary_bg_color)
 times.addEventListener("input", () => {
     time = times.value;
 }, false);
 
 btn.addEventListener("click", function(){
     var place = document.getElementById("selection").value;
-    tg.sendData(time+" "+place);
+    tg.sendData(time+" "+place+" "+tg.secondary_bg_color);
 });
