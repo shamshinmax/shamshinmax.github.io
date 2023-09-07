d73912dd38ade9a5cbb1868978faa4f9a131a508
shamshinmax <94185311+shamshinmax@users.noreply.github.com>
2023-09-07 19:50:09 +0300

    Update app.js

--- script/app.js
+++ script/app.js
@@ -8,6 +8,15 @@ message.addEventListener('input', function handleChange(event) {
   console.log(event.target.value);
 
 });
+function isTextInput(node) {
+    return ['INPUT', 'TEXTAREA'].indexOf(node.nodeName) !== -1;
+}
+
+document.addEventListener('touchstart', function(e) {
+    if (!isTextInput(e.target) && isTextInput(document.activeElement)) {
+        document.activeElement.blur();
+    }
+}, false);
 local.addEventListener('input', function handleChange(event) {
     console.log(event.target.value);
 
